8d467f6b910caec5e8070f5f9d337224c2dd1c8b
Hederssantos <hederssantos@gmail.com>
2022-08-10 13:32:02 +0000

    Testanto CSS

--- app.py
+++ app.py
@@ -8,11 +8,26 @@ def hello():
 
 @app.route("/contatos") 
 def contato():
-    return"""<html>
-	<head>
-		<title> Contatos </title>
-	</head>
-	<body>
-		<h2>Heder Souza dos Santos</h2>
-</html>l"""
+    return"""<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>CSS experiment</title>
+    <style>
+      h1 {
+        color: blue;
+        background-color: yellow;
+        border: 1px solid black;
+      }
+
+      p {
+        color: red;
+      }
+    </style>
+  </head>
+  <body>
+    <h3>Hello World!</h3>
+    <p><h7>This is a CSS example</h7></p>
+  </body>
+</html>"""
 
